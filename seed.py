d0bfd43bdd4023eb23adddf6c91f415b082c42c9
Jamie <jxxthomson@gmail.com>
2025-07-07 11:44:29 +0100

    fixed bugs and managed to make my test data populate and update as required

--- seed.py
+++ seed.py
@@ -1,11 +1,15 @@
-from db import save_user_profile
+from db import save_user_profile, get_user_profile
 import datetime
 
+userID="jamie@perigonpartners.co.uk"
+
 save_user_profile(
-    user_id="jamie@perigonpartners.co.uk",
-    home_postcode="EH39",
+    user_id= userID,
+    home_postcode="SL4 6DW",
     office_postcode="EC1A 1AA",
     created_at=str(datetime.datetime.now())
 )
 
-
+print("✅ User profile inserted.")
+print("🔍 Verifying saved profile...")
+print(get_user_profile(userID))
